309aa7ae0b09717405a9c4a4cde87c7c6ebebf81
shotgun45 <jipeles@yahoo.com>
2025-09-14 21:45:08 -0400

    chapter 2 lesson 1

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	baseURL := args[0]
+	fmt.Printf("starting crawl of: %s\n", baseURL)
 }
